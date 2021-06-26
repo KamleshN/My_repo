b74538d65324573458811490eadae9bc7a530881
Ajeet <ajeet@finflux.co>
2021-06-26 19:44:44 +0530

    navigation done till xlent4

--- app/src/main/java/com/example/xlent_devs/Xlent_Devs2.java
+++ app/src/main/java/com/example/xlent_devs/Xlent_Devs2.java
@@ -1,14 +1,29 @@
 package com.example.xlent_devs;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Xlent_Devs2 extends AppCompatActivity {
+
+    AppCompatButton login_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_xlent__devs2);
+
+        login_btn = findViewById(R.id.login_btn_xlent);
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Xlent_Devs2.this , Xlent_Devs3.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/xlent_devs/Xlent_Devs3.java
+++ app/src/main/java/com/example/xlent_devs/Xlent_Devs3.java
@@ -1,14 +1,29 @@
 package com.example.xlent_devs;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Xlent_Devs3 extends AppCompatActivity {
+
+    AppCompatButton login_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_xlent__devs3);
+
+        login_btn = findViewById(R.id.next_btn);
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Xlent_Devs3.this , Xlent_Devs4.class);
+                startActivity(intent);
+            }
+        });
     }
 }
